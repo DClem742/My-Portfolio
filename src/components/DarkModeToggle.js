8ac38e5f9a2ae6790e413ff2181a6db23639ff96
DC Isenberg <dclem742@gmail.com>
2024-11-26 22:24:27 -0500

    changed DarkMode switch

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,23 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import './DarkModeToggle.css';
 
 const DarkModeToggle = ({ isDark, toggleDark }) => {
   return (
     <div className="dark-mode-toggle">
-      <input
-        type="checkbox"
-        id="dark-mode"
-        checked={isDark}
-        onChange={toggleDark}
+      <FontAwesomeIcon 
+        icon={isDark ? faMoon : faSun}
+        onClick={toggleDark}
+        className="theme-icon"
       />
-      <label htmlFor="dark-mode">
-        <span className="toggle-track">
-          <span className="toggle-indicator">
-            {/* Sun/Moon icons */}
-            {isDark ? '🌙' : '☀️'}
-          </span>
-        </span>
-      </label>
     </div>
   );
 };
